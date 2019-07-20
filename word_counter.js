5f7ce891ece6ebe8569d51683c744f0ece39b804
Xon <tazipper1@hotmail.com>
2019-07-20 09:18:00 +0800

    XF2 stripBbCode for js

--- word_counter.js
+++ word_counter.js
@@ -12,8 +12,111 @@
     $.FE.PLUGINS.charCounter = function(editor) {
         var counter;
 
+        function stripBbCode(text) {
+            var parts = text.split(/(\[quote[^\]]*\]|\[\/quote\])/i);
+            var s = '';
+            var quoteLevel = 0;
+            for (var i = 0; i < parts.length; i++) {
+                var part = parts[i];
+                if (i % 2 === 0) {
+                    // always text, only include if not inside quotes
+                    if (quoteLevel === 0) {
+                        s = s + part.replace(/\s+$/, '') + "\n";
+                    }
+                } else {
+                    // quote start/end
+                    if (part[1] === '/') {
+                        // close tag, down a level if open
+                        if (quoteLevel) {
+                            quoteLevel--;
+                        }
+                    } else {
+                        // up a level
+                        quoteLevel++;
+                    }
+                }
+            }
+
+            // replaces unviewable tags with a text representation
+            s = s.replace('[*]', '');
+            s = s.replace(/\[(attach|media|img|spoiler|ispoiler)[^]\]*?].*?\[\/\1\]/si, '',);
+
+            // split the string into possible delimiters and text; even keys (from 0) are strings, odd are delimiters
+            parts = s.split(/(\[[a-z0-9_]+(?:=[^]\]*){0,1}?]|\[[a-z0-9_]+(?:s?[a-z0-9_]+="[^"]*")+\]|\[\/[a-z0-9_]+\])/si);
+            if (parts.length < 2) {
+                return s;
+            }
+
+            var closes = {};
+            var skips = {};
+
+            // first pass: find all the closing tags and note their keys
+            for (i = 1; i < parts.length; i += 2)
+            {
+                var match = parts[i].match(/^\[\/([a-z0-9_]+)\]/i);
+                if (match)
+                {
+                    var k = match[1].toLowerCase();
+                    if (!closes[k])
+                    {
+                        closes[k] = [];
+                    }
+                    closes[k].push(i);
+                }
+            }
+
+            var cleaned = '';
+
+            // second pass: look for all the text elements and any opens, then find
+            // the first corresponding close that comes after it and remove it.
+            // if we find that, don't display the open or that close
+            for (i = 0; i < parts.length; i++)
+            {
+                part = parts[i];
+                if (i % 2 === 0)
+                {
+                    // string part
+                    cleaned = cleaned + part;
+                    continue;
+                }
+
+                if (i in skips)
+                {
+                    // known close
+                    continue;
+                }
+
+                match = part.match(/^\[([a-z0-9_]+)(?:=|s?[a-z0-9_]+="[^"]*"|\])/i);
+                if (match)
+                {
+                    var tagName = match[1].toLowerCase();
+                    if (closes[tagName])
+                    {
+                        var closeKey;
+                        do
+                        {
+                            closeKey = closes[tagName].shift();
+                        }
+                        while (closes[tagName].length && closeKey < i);
+                        if (closeKey)
+                        {
+                            // found a matching close after this tag
+                            skips[closeKey] = true;
+                            continue;
+                        }
+                    }
+                }
+
+                cleaned = cleaned + part;
+            }
+
+            return cleaned;
+        }
+
         function countWords() {
             var text = editor.el.innerText || "";
+
+            text = stripBbCode(text);
             text = text.replace(/\s+/gi, " ");
             text = text.trim();
             return text.length ? text.split(' ').length : 0;
