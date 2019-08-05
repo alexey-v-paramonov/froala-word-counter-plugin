55f6aafb758b93b0ac52adfe9f5b5d57202fa668
Xon <tazipper1@hotmail.com>
2019-08-05 10:49:23 +0800

    only recompute time at most every second, and rename things to avoid name-clashes

--- word_counter.js
+++ word_counter.js
@@ -6,11 +6,12 @@
 
 (function ($) {
     $.extend($.FE.DEFAULTS, {
-        wordCounter: false,
+        xfSvWordCounter: false,
     });
 
-    $.FE.PLUGINS.charCounter = function(editor) {
+    $.FE.PLUGINS.xfSvWordCounter = function(editor) {
         var counter;
+        var timeout;
 
         function stripBbCode(text) {
             var parts = text.split(/(\[quote[^\]]*\]|\[\/quote\])/i);
@@ -125,30 +126,39 @@
             return text.length ? text.split(' ').length : 0;
         }
 
-        function updateCounter() {
-            if (editor.opts.wordCounter) {
-                var text = countWords() + " " + (editor.opts.wordCounterLabel || "words");
-                counter.text(text);
-                editor.opts.toolbarBottom && counter.css("margin-bottom", editor.$tb.outerHeight(!0));
-                var t = editor.$wp.get(0).offsetWidth - editor.$wp.get(0).clientWidth;
-                0 <= t && ("rtl" === editor.opts.direction ? counter.css("margin-left", t) : counter.css("margin-right", t))
+        function updateCounterDelay() {
+            if (editor.opts.xfSvWordCounter && !timeout) {
+                timeout = setTimeout(updateCounter, 1000);
             }
         }
+
+        function updateCounter() {
+            var text = countWords() + " " + (editor.opts.xfSvWordCounterLabel || "words");
+            counter.text(text);
+            editor.opts.toolbarBottom && counter.css("margin-bottom", editor.$tb.outerHeight(!0));
+            var t = editor.$wp.get(0).offsetWidth - editor.$wp.get(0).clientWidth;
+            0 <= t && ("rtl" === editor.opts.direction ? counter.css("margin-left", t) : counter.css("margin-right", t))
+        }
+
         return {
             _init: function() {
-                if(!!editor.opts.wordCounter && !!editor.$wp){
-                    counter = $('<span class="fr-counter"></span>').css("bottom", editor.$wp.css("border-bottom-width"));
+                if(!!editor.opts.xfSvWordCounter && !!editor.$wp){
+                    counter = $('<span class="fr-word-counter"></span>').css("bottom", editor.$wp.css("border-bottom-width"));
 
                     editor.$box.append(counter);
-                    editor.events.on("paste.afterCleanup", updateCounter);
-                    editor.events.on("contentChanged", updateCounter);
-                    editor.events.on("charCounter.update", updateCounter);
-                    editor.events.on("html.set", updateCounter);
+                    editor.events.on("paste.afterCleanup", updateCounterDelay);
+                    editor.events.on("contentChanged", updateCounterDelay);
+                    editor.events.on("charCounter.update", updateCounterDelay);
+                    editor.events.on("html.set", updateCounterDelay);
                     editor.events.trigger("charCounter.update");
                     editor.events.on("destroy", function() {
                         $(editor.o_win).off("resize.char" + editor.id);
                         counter.removeData().remove();
-                        counter = null
+                        counter = null;
+                        if (timeout) {
+                            clearTimeout(timeout);
+                            timeout = null;
+                        }
                     });
                 }
             },
